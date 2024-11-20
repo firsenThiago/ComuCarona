5067a8a1b8ddffabe5fd418427768d28a2a0fc25
Thiago Firsen <thiagofirsen47@hotmail.com>
2024-11-20 18:58:52 -0300

    Initial Commit

---
+++ ComuCarona/ComuCaronaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComuCaronaApp.swift
+//  ComuCarona
+//
+//  Created by Thiago Firsen on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ComuCaronaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
