b422562f0b3d95b0daf4fcc70515ce40b1e90937
Thiago Firsen <thiagofirsen47@hotmail.com>
2024-11-29 23:08:59 -0300

    feat: uitoast

---
+++ ComuCarona/Shared/Components/UIToast.swift
@@ -0,0 +1,55 @@
+//
+//  UIToast.swift
+//  ComuCarona
+//
+//  Created by Thiago Firsen on 29/11/24.
+//
+
+import SwiftUI
+
+enum ToastState {
+    case warning
+    case error
+}
+
+struct UIToast: View {
+    var title: String
+    var state: ToastState
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 8) {
+            Image(systemName: "exclamationmark.circle")
+                .foregroundColor(.basic(.white))
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.basic(.white))
+            
+        }
+        .padding(15)
+        .background(currentBackgroundColor)
+        .cornerRadius(8)
+    }
+    
+    private var currentBackgroundColor: Color {
+        switch state {
+        case .warning:
+            return .theme(.secondary)
+        case .error:
+            return .feedback(.error)
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        UIToast(
+            title: "Todas as vagas dessa carona ja foram preechidas! 😥",
+            state: .warning
+        )
+        UIToast(
+            title: "Ocorreu problema ao salvar a carona. Por favor, tente novamente!",
+            state: .error
+        )
+    }
+    .padding()
+}
